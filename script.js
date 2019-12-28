2e66eea284e0dcb3116475b9fe40e9e5e26d80e2
Joey Merino <jgamerino@gmail.com>
2019-12-28 06:00:39 -0500

    added current location, temp, humidity, and wind

--- script.js
+++ script.js
@@ -1,21 +1,131 @@
+$(document).ready(function(){
+      //Check geolocation success 
+      if (navigator.geolocation) {
+        console.log('Geolocation API success') 
+
+        // Geolocation API not supported by current browser
+        }  else {
+           console.log('Geolocation API is not supported by your browser')
+           };
+        });
+
+        // Get latitude and longitude
+        navigator.geolocation.getCurrentPosition(function(position){
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+       // $("#City-date").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+
+        console.log("Your latitude is: " + lat + " and your longitude is: " + long);
+
+  
+        var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&APPID=ed2d8b9a647015246d1c2a69c8fa34a3";
+
+       /* var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q="+ data.name + data.sys.country; */
+
+
+      
+        $.ajax({
+        // http:// added
+        url: weatherURL,
+        type: "GET",
+        dataType: "JSON",
+        success: function(data, weatherURL) {
+            $('#city-date').html("City: " + data.name  + ' ' + data.sys.country + ' ' + '<img src=' + data.weather[0].icon +  '>'); 
+         
+            // /$(".icon").html("<img src=' + icon + '>");
+            $('#temp').html("Temp: " + data.main.temp);
+            $('#hum').html("Hum: " + data.main.humidity);
+            $('#wind').html("Wind: " + data.wind.speed);
+            //$('#uv').html("UV Index: " + value);
+            //console.log(uvIndex.value)
+
+
+
+            
+
+            //var city = data.name
+          
+          
+          
+            //$('#city-date').show(position)
+            //citySearch.append(city)
+            
+            //var weather = data.weather[0].description
+            //var temp = data.coord.weather.main.temp
+            //var humidity = data.coord.weather.main.humidity
+            //var windSpeed = data.coord.weather.main.wind.speed
+           // var cityName = data.coord.main.name
+
+
+
+            console.log(position);
+            //console.log(weather);
+           // console.log(temp);
+        
+        },
+    
+
+       
+    }); 
+   
+    /*
+    $.ajax({
+        // http:// added
+        url: fiveDay,
+        type: "GET",
+        dataType: "JSON",
+        success: function(fiveday) {
+
+            $('#5-day').text(fiveDay);
+
+        }
+
+    });
 
     
-    function buildQueryURL() {
-        // queryURL is the url we'll use to query the API
-        var queryURL = "api.openweathermap.org ";
-      
-        // Begin building an object to contain our API call's query parameters
-        // Set the API key
-        var queryParams = { "api-key": "ed2d8b9a647015246d1c2a69c8fa34a3" };
-      
-        // Grab text the user typed into the search input, add to the queryParams object
-        queryParams.q = $("#run-search")
-          .val()
-          .trim();
-      
-        // Logging the URL so we have access to it for troubleshooting
-        console.log("---------------\nURL: " + queryURL + "\n---------------");
-        console.log(queryParams)
-    
-      }
+  
+    /*
+    $.ajax({
+        // http:// added
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=ed2d8b9a647015246d1c2a69c8fa34a3",
+        type: "GET",
+        dataType: "JSON",
+        success: function(data) {
+            var city = data.name
+            //var weather = data.weather[0].description
+            //var temp = data.coord.weather.main.temp
+            //var humidity = data.coord.weather.main.humidity
+            //var windSpeed = data.coord.weather.main.wind.speed
+           // var cityName = data.coord.main.name
 
+
+
+            console.log(city);
+        },
+       
+    }); 
+
+    $.ajax({
+        // http:// added
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=ed2d8b9a647015246d1c2a69c8fa34a3",
+        type: "GET",
+        dataType: "JSON",
+        success: function(data) {
+            //var city = data.name
+            //var weather = data.weather[0].description
+            //var temp = data.coord.weather.main.temp
+            var humidity = data.main.humidity
+            //var windSpeed = data.coord.weather.main.wind.speed
+           // var cityName = data.coord.main.name
+
+
+
+            console.log(humidity);
+        },
+       
+    }); 
+    */
+  
+});
+
+//https://cors-anywhere.herokuapp.com/ 
