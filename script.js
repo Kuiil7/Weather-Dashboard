85faefb7ae5ab3361dc03f45b9aa399ed8c04ec3
Joey Merino <jgamerino@gmail.com>
2019-12-28 08:42:01 -0500

    added 5 day forecast but it comes with three hour each day included.

--- script.js
+++ script.js
@@ -13,17 +13,16 @@ $(document).ready(function(){
         navigator.geolocation.getCurrentPosition(function(position){
         var lat = position.coords.latitude;
         var long = position.coords.longitude;
-       // $("#City-date").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
 
         console.log("Your latitude is: " + lat + " and your longitude is: " + long);
 
-  
+  //API key turned into a var
         var weatherURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=imperial&APPID=ed2d8b9a647015246d1c2a69c8fa34a3";
 
-       /* var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q="+ data.name + data.sys.country; */
+        //var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+long+"";
 
 
-      
+//ajax call for weather  
         $.ajax({
         // http:// added
         url: weatherURL,
@@ -31,56 +30,63 @@ $(document).ready(function(){
         dataType: "JSON",
         success: function(data, weatherURL) {
             $('#city-date').html("City: " + data.name  + ' ' + data.sys.country + ' ' + "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>"); 
-           
-            //$(".icon").html("<img src='http://openweathermap.org/img/w/" //+ data.weather[0].icon + ".png'>");
 
             // /$(".icon").html("<img src=' + icon + '>");
             $('#temp').html("Temp: " + data.main.temp + "°F");
             $('#hum').html("Hum: " + data.main.humidity);
             $('#wind').html("Wind: " + data.wind.speed + ' ' + "MPH");
             //$('#uv').html("UV Index: " + value);
-            //console.log(uvIndex.value)
-
-
-
+           
+            //var row = $('<div class="five-days>').appendTo("body"); 
             
-
-            //var city = data.name
-          
-          
-          
-            //$('#city-date').show(position)
-            //citySearch.append(city)
             
-            //var weather = data.weather[0].description
-            //var temp = data.coord.weather.main.temp
-            //var humidity = data.coord.weather.main.humidity
-            //var windSpeed = data.coord.weather.main.wind.speed
-           // var cityName = data.coord.main.name
-
-
-
-            console.log(position);
-            //console.log(weather);
-           // console.log(temp);
-        
         },
-    
+   
 
        
     }); 
-   
-    /*
+
+    
+    var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+long+"&units=imperial&APPID=ed2d8b9a647015246d1c2a69c8fa34a3";
+    
     $.ajax({
         // http:// added
         url: fiveDay,
         type: "GET",
         dataType: "JSON",
-        success: function(fiveday) {
+        success: function(data) {
 
-            $('#5-day').text(fiveDay);
+             row = $('<div class=".five-days">').appendTo(".five-container");
+            
+            for (i = 5; i < data.list.length; i++) {
+             var row = $(`<div class="row">`);
+
+               var col1 = data.list[i].dt_txt
+               console.log(col1)
+
+               var col2 = "<img src='http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png'>";
+
+               //var col2 =  data.list[i].weather[0].icon;
+               console.log(col2)
+
+               var col3 = data.list[i].main.temp;
+               console.log(col3)
+
+               var col4 =  data.list[i].main.humidity;
+               console.log(col4)
+
+               row.append(col1, col2, col3,col4);
+               console.log(row)
+
+                $(".five-days").append(row);   
+
+//$('.five-day').html(cityId + icon + temp + humidity);
+
+
+
 
         }
+    }
 
     });
 
