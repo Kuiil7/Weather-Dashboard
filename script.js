46e275ed57c27845290b9a179f4d1c990508e55e
Joey Merino <jgamerino@gmail.com>
2019-12-27 23:50:55 -0500

    added temperature, humidity, wind speed, and UV index elements to the body to be ready to be fed by data pulls from API. Also, used CSS to position objects.

--- script.js
+++ script.js
@@ -1,39 +1,21 @@
 
-// A $( document ).ready() block.
-$( document ).ready(function() {
     
+    function buildQueryURL() {
+        // queryURL is the url we'll use to query the API
+        var queryURL = "api.openweathermap.org ";
+      
+        // Begin building an object to contain our API call's query parameters
+        // Set the API key
+        var queryParams = { "api-key": "ed2d8b9a647015246d1c2a69c8fa34a3" };
+      
+        // Grab text the user typed into the search input, add to the queryParams object
+        queryParams.q = $("#run-search")
+          .val()
+          .trim();
+      
+        // Logging the URL so we have access to it for troubleshooting
+        console.log("---------------\nURL: " + queryURL + "\n---------------");
+        console.log(queryParams)
+    
+      }
 
-<button type="button" class="btn btn-primary">Primary</button>
-
-
-$("#currentDate").text(moment().format("LLLL"));
-
-//setting up a loop that correspond to 24 hour format starting from 9hr to 18 hr
-//and begins at 9am and ends at 5pm 
-for (var i = 9; i < 18; i++) {
-
-    // create a row that includes includes the 9 am and 5 pm variable 
-    var row = $(`<div data-time=${i} id='${i}' class="row">`);
-
-    // create a column that includes includes the 9 am and 5 pm variable 
-    var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
-
-    //create column 2 that includes includes the 9 am and 5 pm variable 
-    var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="userinput" placeholder="enter text here..."></textarea>`);
-
-    //create column 3 that includes includes the 9 am and 5 pm variable 
-    var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
-
-    // append col to row that includes includes the 9 am and 5 pm variable 
-    row.append(col1, col2, col3);
-
-    // adding appended columns in rows to main content
-    $(".main-content").append(row);
-
-    //saving time to local storage
-    localStorage.i; 
-
-}
-
-
-});
