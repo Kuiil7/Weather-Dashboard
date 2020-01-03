230f1473e960e06edecc61b4678f19154541dd5e
Joey Merino <jgamerino@gmail.com>
2020-01-03 12:01:18 -0500

    added moments.js. Updated onclick function to include both 5 day forecast and current location data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-   /* //Check geolocation success 
+    /*//Check geolocation success 
     if (navigator.geolocation) {
       console.log('Geolocation API success') 
 
@@ -16,57 +16,81 @@ $(document).ready(function(){
 
       console.log("Your latitude is: " + lat + " and your longitude is: " + long);
 */
-//API key turned into a var
-      //var geoWeather = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=imperial&APPID=ed2d8b9a647015246d1c2a69c8fa34a3";
 
-      //var inputLocation = "http://api.openweathermap.org/data/2.5/weather?q="+ userinputvalue +"&units=imperial&APPID=ed2d8b9a647015246d1c2a69c8fa34a3";
+$('.btn').click(function (){
 
-   // var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+long+"";
+    var input = $("input:text").val();
+    
+    //var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q="+input+"";
+
+    var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?q="+input+"&units=imperial&&cnt=5&APPID=ed2d8b9a647015246d1c2a69c8fa34a3";
+
+    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q="+input+"&units=imperial&APPID=ed2d8b9a647015246d1c2a69c8fa34a3";
+
+    var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?&appid=ed2d8b9a647015246d1c2a69c8fa34a3&q="+input+""
 
 
-
-
-
-    $( ".btn" ).click(function() {
-    var input = $("input:text").val()
-
-   fetch('http://api.openweathermap.org/data/2.5/weather?q='+input+'&units=imperial&APPID=ed2d8b9a647015246d1c2a69c8fa34a3')
-
-   .then(response => response.json()) 
-   .then(data => {
-       
-       $('#city-date').html("<h3>"  + data.name  +' ' + data.sys.country + ' ' + "<img src='http://openweathermap.org/img/w/" +  data.weather[0].icon + ".png'>"); 
-       $('#temp').html("Temp: " + data.main.temp + "°F");
-      
-       $('#hum').html("Hum: " + data.main.humidity);
-      
-       $('#wind').html("Wind: " + data.wind.speed + ' ' + "MPH");
-       //$('#uv').html("UV Index: " + value);
-
-       localStorage.setItem(".btn",(input));
+    localStorage.setItem(".btn",(input));
        var li = "<li>" + input + "</li>";
-        $('.list-group').append(li) 
-        
-   })
-    
+           $('.list-group').append(li) 
 
-   fetch('http://api.openweathermap.org/data/2.5/uvi?'+input+'&units=imperial&APPID=ed2d8b9a647015246d1c2a69c8fa34a3')
+//ajax call for weather  
+      $.ajax({
+      // http:// added
+      url: weatherURL,
+      //type: "GET",
+      dataType: "JSON",
+      success: function(data, weatherURL) {
+        $("#time").text(moment().format("LLLL"));
+          $('#city-date').html("<h3>"  + data.name  +' ' + data.sys.country + ' ' + "<img src='http://openweathermap.org/img/w/" +  data.weather[0].icon + ".png'>"); 
 
-   .then(response => response.json()) 
-   .then(data => {
-       $('#uv').html("UV Index: " + data.date);
+          $('#temp').html("Temp: " + data.main.temp + "°F");
+         
+          $('#hum').html("Hum: " + data.main.humidity);
+         
+          $('#wind').html("Wind: " + data.wind.speed + ' ' + "MPH");
+         
+
+          $.ajax({
+            // http:// added
+            url: fiveDay,
+           // type: "GET",
+            dataType: "JSON",
+            success: function(data) {
+                
+                var weeklyForecast = "";
+                $.each(data.list, function(index, value) {
+                  weeklyForecast += "<p >" // Opening paragraph tag
+                  weeklyForecast += moment().format("LLLL")// Day
+                  weeklyForecast += "<br>" + "<img src='https://openweathermap.org/img/w/" + value.weather[0].icon + ".png'> </br>" // Icon
+                  weeklyForecast += "<br>Temp:"+' '  + value.main.temp + "&degF" + "</br>" ; // Description
+                  weeklyForecast += "<br> Humidity:"+' '  + value.main.humidity + "%"+ "</br>" ; // Description
+                  weeklyForecast += "</p>" // Closing paragraph tag
+                });
+                $(".five-container").html(weeklyForecast);
+
+                $.ajax({
+                    // http:// added
+                    url: uvIndex,
+                   // type: "GET",
+                    dataType: "JSON",
+                    success: function(data) {
+                    $('#uv').html("UV Index: " + value.value);
+
+
+
+                    }
+                        
+                        });
+            
+            }
        
-        
-   })
-    
+          });
+     },   
+  }); 
 
 
-
-
-
-
-
-     });
+});
 
 
 
